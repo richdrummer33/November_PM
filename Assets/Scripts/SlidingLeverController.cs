6b95ea171b33cf67b75289be9d942c1e8ee35ae3
Rich <rsbeare@gmail.com>
2020-02-04 23:26:46 -0500

    End of session 15

---
+++ Assets/Scripts/SlidingLeverController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingLeverController : MonoBehaviour
+{
+    public Transform upperLimit, lowerLimit;
+
+    Vector3 heading;
+    Vector3 normazedHeading;
+    
+    Transform playerHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        heading = upperLimit.position - lowerLimit.position;
+
+        normazedHeading = heading.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerHand != null)
+        {
+            Vector3 startToHand = playerHand.position - lowerLimit.position;
+
+            float dotProduct = Vector3.Dot(startToHand, normazedHeading);
+
+            dotProduct = Mathf.Clamp(dotProduct, 0f, heading.magnitude);
+
+            transform.position = lowerLimit.position + normazedHeading * dotProduct;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<XrGrab>())
+        {
+            playerHand = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<XrGrab>())
+        {
+            playerHand = null;
+        }
+    }
+}
---
+++ Assets/Scripts/WreckingBallExplode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WreckingBallExplode : MonoBehaviour
+{
+    ParticleSystem explosionParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosionParticles = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 1f && explosionParticles.isPlaying == false)
+        {
+            explosionParticles.Play();
+        }
+    }
+}
