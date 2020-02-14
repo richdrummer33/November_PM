50dd6f4985fd1481767cfd40c0e3ede6e7c76556
Rich <rsbeare@gmail.com>
2020-02-13 23:26:16 -0500

    End of session 18

--- Assets/Scripts/ArPrefabInstantiator.cs
+++ Assets/Scripts/ArPrefabInstantiator.cs
@@ -9,25 +9,41 @@ public class ArPrefabInstantiator : MonoBehaviour
 
     GameObject prefabInstance;
 
-    ARRaycastManager raycast; 
+    ARRaycastManager raycastManager; 
     
     void Start()
     {
-        raycast = GetComponent<ARRaycastManager>();
+        raycastManager = GetComponent<ARRaycastManager>();
     }
     
     void Update()
     {
-        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && prefabInstance == null) // Check for screen touch. Remove "&& prefabInstance != null" if you want to instantiate multiple prefabs
+        if (prefabInstance == null)
         {
-            Vector2 touchPoint = Input.GetTouch(0).position;
-
-            List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
-
-            if (raycast.Raycast(touchPoint, hitResults, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) // Check for screen touch. Remove "&& prefabInstance != null" if you want to instantiate multiple prefabs
             {
-                prefabInstance = Instantiate(prefab, hitResults[0].pose.position, prefab.transform.rotation); // hitResults[0].pose.position = the point on the plane that the raycast hit
+                Vector2 touchPoint = Input.GetTouch(0).position;
+
+                List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+
+                if (raycastManager.Raycast(touchPoint, hitResults, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    prefabInstance = Instantiate(prefab, hitResults[0].pose.position, prefab.transform.rotation); // hitResults[0].pose.position = the point on the plane that the raycast hit
+
+                    LookAtPlayer();
+                }
             }
         }
+    }
+
+    void LookAtPlayer() // Tell prefabInstance to look in hte direction of the player (e.g. the Camera.main) 
+    {
+        Vector3 lookDirection = Camera.main.transform.position - prefabInstance.transform.position;
+
+        Quaternion lookRotation = Quaternion.LookRotation(lookDirection); // New rotation facing the phone/player camera
+
+        Vector3 eulerRotation = new Vector3(prefabInstance.transform.eulerAngles.x, lookRotation.eulerAngles.y, prefabInstance.transform.eulerAngles.z); // Cancel out x and z rotation
+
+        prefabInstance.transform.rotation = Quaternion.Euler(eulerRotation); // Finally applyy the rotation
     }
 }
---
+++ Assets/Scripts/CameraReflect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraReflect : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 phoneToMirror = transform.position - Camera.main.transform.position;
+
+        Vector3 reflectDirection = Vector3.Reflect(phoneToMirror, -transform.parent.forward);
+
+        transform.rotation = Quaternion.LookRotation(reflectDirection);
+    }
+}
